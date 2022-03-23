0e714b32ea9303c2c0a888ac18ab272c42b63c4b
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-03-23 15:46:05 +0100

    Add more test specs for `images` option

--- test/spec/abs.js
+++ test/spec/abs.js
@@ -173,4 +173,74 @@ describe('mkabs:', function() {
     })
   });
 
+  it('should implicitly set `images` to TRUE when `imageBase` option is set', function(done) {
+    var source = 'test/fixtures/image.md'
+      , target = 'target/abs.json.log'
+      , data = parser.parse('' + fs.readFileSync(source))
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data.file = source;
+
+    var input = mkast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {input: input, output: output, imageBase: 'http://image-base.com'};
+    
+    mkabs(opts);
+
+    output.once('finish', function() {
+      var result = utils.result(target);
+
+      // open document
+      expect(result[0].type).to.eql(Node.DOCUMENT);
+      // mock document paragraph
+      expect(result[1].type).to.eql(Node.PARAGRAPH);
+
+      var images = collect(result, Node.IMAGE)
+        , slash = images[0];
+
+      expect(slash.destination).to.eql('http://image-base.com/example.png');
+
+      // eof main document
+      expect(result[2].type).to.eql(Node.EOF);
+
+      done();
+    })
+  });
+
+  it('should not handle images when `imageBase` option is set but `images` is explicitly set to FALSE', function(done) {
+    var source = 'test/fixtures/image.md'
+      , target = 'target/abs.json.log'
+      , data = parser.parse('' + fs.readFileSync(source))
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data.file = source;
+
+    var input = mkast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {input: input, output: output, images: false, imageBase: 'http://image-base.com'};
+    
+    mkabs(opts);
+
+    output.once('finish', function() {
+      var result = utils.result(target);
+
+      // open document
+      expect(result[0].type).to.eql(Node.DOCUMENT);
+      // mock document paragraph
+      expect(result[1].type).to.eql(Node.PARAGRAPH);
+
+      var images = collect(result, Node.IMAGE)
+        , slash = images[0];
+
+      expect(slash.destination).to.eql('/example.png');
+
+      // eof main document
+      expect(result[2].type).to.eql(Node.EOF);
+
+      done();
+    })
+  });
+
 });
